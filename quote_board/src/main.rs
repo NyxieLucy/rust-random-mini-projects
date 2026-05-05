35a4859688420a29c61d397f4e5580854dd82d53
NyxieLucy <nyxlucianis@gmail.com>
2026-05-05 14:17:53 +0100

    practicing more borowing, ownership and a loop, by adding a show list function and... a loop

--- quote_board/src/main.rs
+++ quote_board/src/main.rs
@@ -1,10 +1,12 @@
 // standart imports nothing too fancy 
-use std::io::{self, Write};
+use std::{io::{self, Write}};
 
 // a simple struct with string value for quote
+#[derive(Debug)]
 struct Quotes {
     quote : String,
 }
+
 /*  function with parameter that uses '&mut' so that you can modify the vector
 without taking ownership */
 /*i just remembered that i have to mention that the parameter tells the function what to do */
@@ -21,12 +23,25 @@ fn adding_quote(quote_list : &mut Vec<Quotes>) {
      then the value you'd store */
     quote_list.push(new_q);
 }
+fn show_list(quote_list:&Vec<Quotes>){
+    print!("{:?}", quote_list);
+}
 
 fn main() {
     // that's the vector, nothing too wow, if ya already know the basics you won't need this line you're reading now
     let mut quote_list: Vec<Quotes> = Vec::new();
+    let mut num_of_quotes_added_str = String::new();
+    print!("how many quotes ya wanna add: ");
+    io::stdout().flush().expect("unvalid");
+    io::stdin().read_line(&mut num_of_quotes_added_str);
     //here we called the function...
-    adding_quote(&mut quote_list);
-    // and finally print the output
-    println!("the first quote is: {}", quote_list[0].quote)
+    let num_quotes = num_of_quotes_added_str.trim().parse().expect("unvalid input");
+    for mut i in 0 .. num_quotes {
+        i = i +1;
+
+        println!("adding quote number {}: ", i);
+        
+        adding_quote(&mut quote_list)
+    }
+    show_list(&quote_list);
 }
