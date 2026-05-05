b8417d4a877378fe6f6c9a6196294930f55369f0
NyxieLucy <nyxlucianis@gmail.com>
2026-05-05 13:31:15 +0100

    fixed the trim issue and the stdin and out thingy

--- quote_board/src/main.rs
+++ quote_board/src/main.rs
@@ -10,11 +10,13 @@ without taking ownership */
 /*i just remembered that i have to mention that the parameter tells the function what to do */
 fn adding_quote(quote_list : &mut Vec<Quotes>) {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("failed to read");
+    println!("what's the quote you wanna add?: ");
     io::stdout().flush().expect("failed to flush");
-    
+// also mind keeping stdout before the stdin so that you can see the output and stuff
+    io::stdin().read_line(&mut input).expect("failed to read");
+
     // added an instance that links the value of the Quotes struct with out input
-    let new_q = Quotes{quote: input};
+    let new_q = Quotes{quote: input.trim().to_string()}; // okay i added the trim so that we get rid of the '/n' annoying and 'to_string' to consider it a string again
     /* and finaly adding it to the vector, remember you write the place where to store
      then the value you'd store */
     quote_list.push(new_q);
