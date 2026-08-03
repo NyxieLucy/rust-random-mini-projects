03a22397129e394a24893cbf5604201328510849
NyxieLucy <nyxlucianis@gmail.com>
2026-08-03 21:36:30 +0100

    man idk what i'm doing

--- to_do_list/src/structs.rs
+++ to_do_list/src/structs.rs
@@ -1,8 +1,12 @@
 use chrono::{DateTime, Utc};
+use json::JsonValue;
 use serde_json::{Deserializer, Serializer, json};
 #[derive(Debug)]
 pub struct Task{
     pub title: String,
     pub date: String,
     pub checked : bool,
+}
+pub fn to_json() -> JsonValue {
+    let mut obj = json::JsonValue
 }
