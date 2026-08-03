0cc48c8528a3405a570af4c4ad3e20539fa06aaa
NyxieLucy <nyxlucianis@gmail.com>
2026-08-03 17:30:37 +0100

    okay, adding some advanced stuff and adjustmets

--- to_do_list/src/main.rs
+++ to_do_list/src/main.rs
@@ -2,20 +2,22 @@ use std::env;
 use std::string::String;
 
 
-use chrono::{DateTime};
+use chrono::{DateTime, Utc};
 
 use crate::structs::Task;
 mod structs;
 fn main() {
     
     let mut list : Vec<String> = Vec::new();
-     let new_task_title:String =  env::args().skip(1).collect();
+     let new_task_title:String =  env::args().skip(1).collect::<Vec<String>>().join(" ");
+    let new_date = Utc::now().to_string();
     let new_Task = Task{
         title : new_task_title,
+        date : new_date, 
         checked : false,
 
     };
-    for i in 1..list.len(){
-        println!("the item number {i} is {:?}\n ", list[i]);
+    for (i, task) in list.iter().enumerate(){
+        println!("the item number {} is {:?}\n ",i + 1,  new_Task.title);
     }
 }
--- to_do_list/src/structs.rs
+++ to_do_list/src/structs.rs
@@ -1,6 +1,8 @@
 use chrono::{DateTime, Utc};
+use serde_json::{Deserializer, Serializer, json};
 #[derive(Debug)]
 pub struct Task{
     pub title: String,
+    pub date: String,
     pub checked : bool,
 }
