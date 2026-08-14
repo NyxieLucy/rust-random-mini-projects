22143d431924eb81e54cc3dc114661a1f9eb8397
NyxieLucy <nyxlucianis@gmail.com>
2026-08-14 15:46:45 +0100

    finally done with the json, slow and steady wins the race

--- to_do_list/src/main.rs
+++ to_do_list/src/main.rs
@@ -2,22 +2,21 @@ use std::env;
 use std::string::String;
 
 
-use chrono::{DateTime, Utc};
+use chrono::{Utc};
 
 use crate::structs::Task;
 mod structs;
 fn main() {
-    
-    let mut list : Vec<String> = Vec::new();
+   let mut list:Vec<Task> = Vec::new(); 
      let new_task_title:String =  env::args().skip(1).collect::<Vec<String>>().join(" ");
     let new_date = Utc::now().to_string();
-    let new_Task = Task{
+    let new_task = Task{
         title : new_task_title,
         date : new_date, 
         checked : false,
-
     };
+    list.push(new_task);
     for (i, task) in list.iter().enumerate(){
-        println!("the item number {} is {:?}\n ",i + 1,  new_Task.title);
+        println!("item #{}: {} [{}]", i + 1, task.title, task.date);
     }
 }
--- to_do_list/src/structs.rs
+++ to_do_list/src/structs.rs
@@ -1,8 +1,7 @@
 use json::JsonValue;
-use std::fs::File;
-use std::io::Write;
-use serde_json::{Deserializer, Serializer};
-#[derive(Debug)]
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Task{
     pub title: String,
     pub date: String,
@@ -10,21 +9,13 @@ pub struct Task{
 }
 // adding an impl to add functionalitites to the type we're tring to make 
 impl Task {
-    // i'm lwk tryna make the added data into json value 
-        pub fn to_json(&mut self) -> JsonValue {
-        let mut obj = JsonValue::new_object();
-        obj["title"] = self.name.clone().into();
-        obj["date"] = self.description.clone().into();
+    // i'm lwk tryna make the added data into json value
+    pub fn to_json(&self) -> Value {
+        json!({
+            "title" :self.title,
+            "date" : self.date,
+            "checked" : self.checked,
+        })
     }
-
-pub fn save_tasks(tasks: &[Task]) -> Result<(), std::io::Error> {
-    let mut json_array = JsonValue::new_array();
-    for task in tasks {
-        let _ = json_array.push(task.to_json());
-    }
-    let mut file = File::create("tasks.json")?;
-    file.write_all(json_array.dump().as_bytes())?;
-    Ok(())
-}
 }
 
