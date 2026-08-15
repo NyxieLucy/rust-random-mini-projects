488ead9a5c65ddd83b2ee5c57646f3583f2586a7
NyxieLucy <nyxlucianis@gmail.com>
2026-08-15 15:36:42 +0100

    added persistence memory via json

--- to_do_list/src/main.rs
+++ to_do_list/src/main.rs
@@ -1,22 +1,33 @@
 use std::env;
 use std::string::String;
+use std::fs::File;
+use std::io::{BufWriter, Write};
 
 
 use chrono::{Utc};
 
 use crate::structs::Task;
 mod structs;
-fn main() {
-   let mut list:Vec<Task> = Vec::new(); 
-     let new_task_title:String =  env::args().skip(1).collect::<Vec<String>>().join(" ");
+fn main() -> std::io::Result<()> {
+    let mut list: Vec<Task> = std::fs::read_to_string("storage.json").ok().and_then(|content| serde_json::from_str(&content).ok()).unwrap_or_else(Vec::new);
+
+    let new_task_title: String = env::args().skip(1).collect::<Vec<String>>().join(" ");
     let new_date = Utc::now().to_string();
-    let new_task = Task{
-        title : new_task_title,
-        date : new_date, 
-        checked : false,
+    let new_task = Task {
+        title: new_task_title,
+        date: new_date,
+        checked: false,
     };
     list.push(new_task);
-    for (i, task) in list.iter().enumerate(){
+
+    for (i, task) in list.iter().enumerate() {
         println!("item #{}: {} [{}]", i + 1, task.title, task.date);
     }
+
+    let storage = File::create("storage.json").unwrap();
+    let mut writer = BufWriter::new(storage);
+    serde_json::to_writer(&mut writer, &list)?;
+    writer.flush()?;
+
+    Ok(())
 }
--- to_do_list/src/structs.rs
+++ to_do_list/src/structs.rs
@@ -1,21 +1,7 @@
-use json::JsonValue;
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Task{
     pub title: String,
     pub date: String,
     pub checked : bool,
 }
-// adding an impl to add functionalitites to the type we're tring to make 
-impl Task {
-    // i'm lwk tryna make the added data into json value
-    pub fn to_json(&self) -> Value {
-        json!({
-            "title" :self.title,
-            "date" : self.date,
-            "checked" : self.checked,
-        })
-    }
-}
-
